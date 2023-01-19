f96796828b747163105ee4c719198117a9452703
usman508 <117597137+usman508@users.noreply.github.com>
2023-01-19 21:42:36 +0500

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,45 @@
+
+terminal_states = {'211111': 0, '22111': 1, '2221': 0}
+
+successors = {
+	'7': ['61', '52', '43'],
+	'61': ['511', '421'],
+        '52' : ['421','322'],
+        '43' : ['421','331'],
+        '511' : ['4111','3211'],
+        '421' : ['3211'],
+        '322' : ['2221'],
+        '331' : ['3211'],
+        '4111' : ['31111'],
+        '3211' : ['22111'],
+        '31111' : ['211111']
+	# Complete this dictionary here
+}
+
+def util_value(state, agent):
+        
+        if state in terminal_states:
+                return(terminal_states[state])
+
+        if agent == 'MAX':
+                return max_value(state)
+
+        if agent == 'MIN':
+                return min_value(state)
+
+def min_value(state):
+        v=1000000000
+        for each in successors[state]:
+                v = min(v, util_value(each, 'MAX'))
+        return v
+
+def max_value(state):
+        v=-1000000000
+        for each in successors[state]:
+                v = max(v, util_value(each, 'MIN'))
+        return v
+	
+
+if __name__ == "__main__":
+	print(util_value('7', 'MIN'))
+	# OUTPUT: 1
